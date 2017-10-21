f08a031939bf1c09d91793fd07994d3cbc7af057
VelkeiMiklos <velkei18@gmail.com>
2017-10-21 21:35:14 +0200

    create VC-s

---
+++ Chat/View/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Chat
+//
+//  Created by Velkei Miklós on 2017. 10. 21..
+//  Copyright © 2017. NeonatCore. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+    
+    override func awakeFromNib() {
+        setupView()
+    }
+    func setupView()
+    {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+    
+}
